16d320a5972ff255890d9bb81d325f9775a8272d
norichin-dev <norimax777@gmail.com>
2025-11-28 11:08:10 +0900

    aaaaaaaaaaaaaaaaaaaaaaaa

--- gpu_cpu_torch.py
+++ gpu_cpu_torch.py
@@ -1,9 +1,5 @@
 import tensorflow as tf
 
-device = "/GPU:0" if tf.config.list_physical_devices("GPU") else "/CPU:0"
+devices = tf.config.list_physical_devices("GPU")
+device = "/GPU:0" if devices else "/CPU:0"
 print("Using:", device)
-
-with tf.device(device):
-    x = tf.random.normal((1000, 1000))
-    y = tf.matmul(x, x)
-print("OK")
